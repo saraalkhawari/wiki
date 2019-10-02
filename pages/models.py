e6a863b9082cae09512c12f7cbadbfa1e6776a4a
Sara Alkhawari <sara.a.alkhawari@gmail.com>
2019-10-02 20:54:34 +0300

    Project WIKI first Commit

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Page(models.Model): 
+	title = models.CharField(max_length = 100)
+	content = models.TextField()
+	last_updated = models.DateTimeField(auto_now = True)
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self): # new
+		return "/detail/%i" % self.id
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Page
 
 # Create your views here.
+
+def page_list(request):
+    context = {
+        "page": Page.objects.all(),
+    }
+    return render(request, 'list.html', context)
+
+
+def page_detail(request, page_id):
+    context = {
+        "page": Page.objects.get(id = page_id),
+    }
+    return render(request, 'details.html', context)
